ea1e6472fd28497d4a66c8be7d37f9051eba1a16
Luke Cartwright <lukeecartwright@gmail.com>
2026-07-07 22:21:16 +0100

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/timelesstruth/',
+  base: '/timeless-truth/',
   plugins: [
     react(),
     tailwindcss()
